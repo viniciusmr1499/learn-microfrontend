c00a9c318f288d659dd8c479e3635ed927e6fabc
Ruan Aragao <aragao.ruan@gmail.com>
2021-07-29 10:49:51 -0300

    implemantando dashboard

--- cockpit/pages/_app.tsx
+++ cockpit/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- cockpit/pages/dashboard/index.tsx
+++ cockpit/pages/dashboard/index.tsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from 'react';
 import { GetServerSideProps } from 'next';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
 
 const getToken = (await import('auth/GetToken')).default;
 
-type User = {
-  name: string;
-  email: string;
-};
-
 const Dashboard: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
@@ -24,22 +33,54 @@ const Dashboard: React.FC = () => {
     get();
   }, []);
 
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <p>Nome: {user?.name}</p>
-          <p>Email: {user?.email}</p>
-        </>
-      )}
-    </div>
+    <>
+      <div className="container-fluid m-0 p-0">
+        <Navbar color="secondary navbar-dark p-4" light expand="md">
+          <NavbarBrand href="#">Midway</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="#">Serviços</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Páginas</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+            <NavbarText style={{ marginLeft: 'auto' }}>
+              <a href="#" style={{ textDecoration: 'none' }}>
+                Sair
+              </a>
+            </NavbarText>
+          </Collapse>
+        </Navbar>
+      </div>
+
+      <div>
+        {loading ? (
+          <div>Loading</div>
+        ) : (
+          <>
+            <p>Nome: {user?.name}</p>
+            <p>E-mail: {user?.email}</p>
+          </>
+        )}
+      </div>
+    </>
   );
 };
-
-export default Dashboard;
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const token = await getToken(ctx);
@@ -57,3 +98,5 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     props: {},
   };
 };
+
+export default Dashboard;
