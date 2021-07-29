d2d1cedc661a91063a4d41e79e6053d4979e28fb
Marcos Vinicius <marcos.oliveira.e@midway.com.br>
2021-07-29 10:06:23 -0300

    resolve conflitos

--- auth/pages/_app.tsx
+++ auth/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
