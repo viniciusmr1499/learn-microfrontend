cbc45ae381a0553daacfa1cc654b541b1c41a63a
Ruan Aragao <aragao.ruan@gmail.com>
2021-07-29 10:51:03 -0300

    implemantando dashboard

--- auth/pages/_app.tsx
+++ auth/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
